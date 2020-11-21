04c00c3573271820affc516f4d82299fd0473ae1
Tudor Roman <tudor@tudorr.ro>
2020-11-21 15:54:56 +0200

    nix: added vendorSha256

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ buildGoModule rec {
   src = ./.;
 
   modSha256 = "0f1q730wp3rhznbfb67ym4gqw8xflcvqh5k8i5hc2rg7xdr41iwg";
+  vendorSha256 = "05j01gyqzgg8wgdr2xhdby3chkph3lmrmlwn225zdb2bvqmiyfq0";
 
   meta = with stdenv.lib; {
     description = "Simple, fast and flexible static site generator written in Go.";
