5568c5da06d8bd11ed309d61187bf4c4dd3f30e7
Tudor Roman <tudor@tudorr.ro>
2021-12-28 13:02:49 +0200

    default.nix: fix deprecation

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
   modSha256 = "0f1q730wp3rhznbfb67ym4gqw8xflcvqh5k8i5hc2rg7xdr41iwg";
   vendorSha256 = "05j01gyqzgg8wgdr2xhdby3chkph3lmrmlwn225zdb2bvqmiyfq0";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Simple, fast and flexible static site generator written in Go.";
     homepage = "https://github.com/tudurom/orator";
     maintainers = [ maintainers.tudorr ];
